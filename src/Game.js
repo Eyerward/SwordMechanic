b354b569f577108837bc1d355f615c54eaa386b6
Eyerward <matthieubruffa@gmail.com>
2022-03-14 15:56:39 +0100

    debug

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,15 @@
 let gameConfig = {
     type: Phaser.AUTO,
-    width: 500,
+    width: 800,
     height: 500,
     backgroundColor: '#000000',
     parent: 'game',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
-        }
+            gravity: {y: 200},
+            debug: true,
+        },
     },
     scene: {
         preload,
@@ -16,19 +17,20 @@ let gameConfig = {
         update,
     },
 };
-let game = new Phaser.Game(gameConfig);
+const game = new Phaser.Game(gameConfig);
 
-class Tableau extends Phaser.scene{
-
-    preload()
+    function preload()
     {
         this.load.image('body','asset/square.png');
         this.load.image('sword','asset/sword.png');
     }
-    create()
+    function create()
     {
         this.body=this.add.image(250,250,'body').setOrigin(0,0);
+        this.body.setDisplaySize(100,250);
+
+
     }
-    update()
+    function update()
     {}
-}
+
