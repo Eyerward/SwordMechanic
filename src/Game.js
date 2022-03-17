a297f089396ad5670ebb0f4b2de284747d3ab425
Eyerward <92451560+Eyerward@users.noreply.github.com>
2022-03-17 10:22:10 +0100

    corps et cible

--- src/Game.js
+++ src/Game.js
@@ -26,12 +26,35 @@ const game = new Phaser.Game(gameConfig);
     }
     function create()
     {
-        this.body=this.add.image(250,250,'body').setOrigin(0,0);
-        this.body.setDisplaySize(100,250);
+        this.hauteur = 720;
+        this.largeur = 1280;
 
-        this.sword=this.add.image(250,250,'sword').setOrigin(0,0);
+        this.warrior = this.add.container
+
+        this.knight = this.physics.add.image(0,300,'body').setOrigin(0,0);
+        this.knight.setDisplaySize(150,400);
+        this.knight.setTintFill(0x0f00ff);
+        this.knight.body.setAllowGravity(false);
+        this.knight.setImmovable(true);
+
+        this.sword = this.physics.add.image(200,100,'sword').setOrigin(0,0);
+        this.sword.setScale(0.5);
+        this.sword.body.setAllowGravity(false);
+        this.sword.setImmovable(true);
+
+        this.target = this.physics.add.image(1000, 300, 'body').setOrigin(0,0);
+        this.target.body.setAllowGravity(false);
+        this.target.setImmovable(true);
 
     }
+
+    function disparait(obstacle){
+
+        obstacle.body.setEnable(false);
+        obstacle.setVisible(false);
+        obstacle.ombre.setVisible(false);
+    }
+
     function update()
     {}
 
