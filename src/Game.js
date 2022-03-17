089d1b93e6940e8c1152ded0d09cedc3014c9a72
Eyerward <92451560+Eyerward@users.noreply.github.com>
2022-03-17 12:59:31 +0100

    Try Sprites

--- src/Game.js
+++ src/Game.js
@@ -29,22 +29,32 @@ const game = new Phaser.Game(gameConfig);
         this.hauteur = 720;
         this.largeur = 1280;
 
-        this.warrior = this.add.container
+        this.warrior = this.add.container(0,0);
 
         this.knight = this.physics.add.image(0,300,'body').setOrigin(0,0);
         this.knight.setDisplaySize(150,400);
         this.knight.setTintFill(0x0f00ff);
         this.knight.body.setAllowGravity(false);
         this.knight.setImmovable(true);
+        this.warrior.add(this.knight);
 
-        this.sword = this.physics.add.image(200,100,'sword').setOrigin(0,0);
+        this.sword = this.physics.add.image(100,100,'sword').setOrigin(0,0);
         this.sword.setScale(0.5);
         this.sword.body.setAllowGravity(false);
         this.sword.setImmovable(true);
+        this.warrior.add(this.sword);
 
         this.target = this.physics.add.image(1000, 300, 'body').setOrigin(0,0);
         this.target.body.setAllowGravity(false);
         this.target.setImmovable(true);
+
+        let me = this;
+
+        this.input.keyboard.on('keydown-D', function (event)
+        {
+            me.warrior.body.setVelocityX(100);
+        });
+
 
     }
 
